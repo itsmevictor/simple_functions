073512b1a6ac712eb03481e97f1f3bac4a950972
itsmevictor <83669556+ItsMeVictor@users.noreply.github.com>
2024-07-10 17:37:07 +0200

    Removed sample_df, modified find_multiple to put n_count as the first column in output df

--- find_multiple.R
+++ find_multiple.R
@@ -3,7 +3,8 @@ find_multiple <- function(df, group_var) {
         group_by({{ group_var }}) |>
         mutate(n_count = n()) |>
         ungroup() |>
-        filter(n_count != 1)
+        filter(n_count != 1) |>
+        select(n_count, everything())
     
     return(df)
 }
--- sample_df.R
+++
@@ -1,3 +0,0 @@
-samp <- function(df, n_rows) {
-    df |> slice_sample(n = n_rows)
-}
