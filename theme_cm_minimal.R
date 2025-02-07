88caa1c9e55f27b11b3a7108a0ab87fe55df4c3f
itsmevictor <83669556+ItsMeVictor@users.noreply.github.com>
2025-02-07 12:34:41 +0100

    added better-looking ggplot theme

---
+++ theme_cm_minimal.R
@@ -0,0 +1,7 @@
+sysfonts::font_add("CMU Sans Serif", "/Users/victorkreitmann/Library/Fonts/cmunss.ttf") # Ensure the path points to the correct font file
+showtext::showtext_auto()
+
+theme_cm_minimal <- function() {
+    theme_minimal() +
+        theme(text = element_text(family = "CMU Sans Serif"))
+}
