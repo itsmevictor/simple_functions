3b18a55d54bb9b4b82d2b423a5f858924085834a
itsmevictor <83669556+ItsMeVictor@users.noreply.github.com>
2025-02-22 21:20:42 +0100

    fixed error

--- check_missing.R
+++ check_missing.R
@@ -1,6 +1,6 @@
 check_missing <- function(df) {
     df |>
         dplyr::summarise(across(everything(), ~ sum(is.na(.x)))) |>
-        dplyr::pivot_longer(everything(), names_to = "variable", values_to = "n_missing") |>
+        tidyr::pivot_longer(everything(), names_to = "variable", values_to = "n_missing") |>
         dplyr::arrange(desc(n_missing))
 }
--- myfunctions/R/check_missing.R
+++ myfunctions/R/check_missing.R
@@ -1,6 +1,6 @@
 check_missing <- function(df) {
     df |>
-        summarise(across(everything(), ~ sum(is.na(.x)))) |>
-        pivot_longer(everything(), names_to = "variable", values_to = "n_missing") |>
-        arrange(desc(n_missing))
+        dplyr::summarise(across(everything(), ~ sum(is.na(.x)))) |>
+        tidyr::pivot_longer(everything(), names_to = "variable", values_to = "n_missing") |>
+        dplyr::arrange(desc(n_missing))
 }
